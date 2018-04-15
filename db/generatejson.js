c482786b25a80e2b90ef36447be385a517303f01
Maëlys McArdle <airseal@hotmail.com>
2018-04-15 10:07:14 -0400

    Updated generate json script to store locally.

--- db/generatejson.js
+++ db/generatejson.js
@@ -1,12 +1,29 @@
 const fetch = require('node-fetch');
 const zlib = require('zlib');
 const datalib = require('datalib');
+const fs = require('fs');
 
 const generateJson = async () => {
   const url = 'https://datasets.imdbws.com/title.basics.tsv.gz';
-  const imdbDb = await downloadImdbDatabase(url);
+  const filePath = 'imdb.tsv';
+
+  const imdbDb = await getImdbDatabase(url, filePath);
   const parsedImdbDb = parseTsvData(imdbDb);
 };
+
+async function getImdbDatabase(url, localDatabasePath)
+{
+  if (!fs.existsSync(localDatabasePath)) {
+    // No local copy of database. Download it and save it to file.
+    const imdbDatabase = downloadImdbDatabase(url);
+    fs.writeFileSync(localDatabasePath, imdbDatabase);
+    return imdbDatabase;
+  } else {
+    // Local copy of database. Use it.
+    const imdbDatabase = fs.readFileSync(localDatabasePath).toString();
+    return imdbDatabase;
+  }
+}
 
 async function downloadImdbDatabase(url)
 {
