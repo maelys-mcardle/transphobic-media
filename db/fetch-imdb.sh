d7ed5db6a3a2a2189b445c4b18cadc6132f6e8f8
Maëlys McArdle <airseal@hotmail.com>
2018-04-15 11:28:44 -0400

    Parsing IMDB lines.

--- db/fetch-imdb.sh
+++
@@ -1,2 +0,0 @@
-wget https://datasets.imdbws.com/title.basics.tsv.gz
-gunzip title.basics.tsv.gz
--- db/generatejson.js
+++ db/generatejson.js
@@ -4,18 +4,49 @@ const datalib = require('datalib');
 const fs = require('fs');
 
 const generateJson = async () => {
-  const url = 'https://datasets.imdbws.com/title.basics.tsv.gz';
-  const filePath = 'imdb.tsv';
+  const remoteImdbUrl = 'https://datasets.imdbws.com/title.basics.tsv.gz';
+  const localImdbPath = 'db/imdb.tsv';
+  const transphobiaDb = 'db/transphobic.tsv';
 
-  const imdbDb = await getImdbDatabase(url, filePath);
-  const parsedImdbDb = parseTsvData(imdbDb);
+  const rawImdbDb = await getImdbDatabase(remoteImdbUrl, localImdbPath);
+  const parsedTransphobiaDb = parseTsvData(transphobiaDb);
+  combineImdbTransphobiaData(rawImdbDb, parsedTransphobiaDb);
 };
+
+function combineImdbTransphobiaData(rawImdbDb, parsedTransphobiaDb)
+{
+  // Get header.
+  let [line, linePosition] = getNextLine(rawImdbDb, 0);
+  console.log(line);
+
+  // Get entries.
+  while (linePosition > 0) {
+    [line, linePosition] = getNextLine(rawImdbDb, linePosition + 1);
+    let [tconst,
+      titleType,
+      primaryTitle,
+      originalTitle,
+      isAdult,
+      startYear,
+      endYear,
+      runtimeMinutes,
+      genres] = line.split('\t');
+    console.log(tconst);
+  }  
+}
+
+function getNextLine(fileContents, startPosition)
+{
+  const endPosition = fileContents.indexOf('\n', startPosition);
+  const line = fileContents.slice(startPosition, endPosition);
+  return [line, endPosition];
+}
 
 async function getImdbDatabase(url, localDatabasePath)
 {
   if (!fs.existsSync(localDatabasePath)) {
     // No local copy of database. Download it and save it to file.
-    const imdbDatabase = downloadImdbDatabase(url);
+    const imdbDatabase = await downloadImdbDatabase(url);
     fs.writeFileSync(localDatabasePath, imdbDatabase);
     return imdbDatabase;
   } else {
