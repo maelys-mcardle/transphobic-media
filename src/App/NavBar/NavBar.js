b7ca4205a80892b927245f653429dea178b67c19
Maëlys McArdle <airseal@hotmail.com>
2018-04-23 19:35:20 -0400

    Fixed navbar url.

--- src/App/NavBar/NavBar.js
+++ src/App/NavBar/NavBar.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './NavBar.css';
 
 export default class NavBar extends Component {
-  
+
   constructor(props) {
+    super(props);
     this.criteriaUrl = 
       "https://github.com/maelys-mcardle/transphobic-media/" +
       "blob/master/db/README.md";
