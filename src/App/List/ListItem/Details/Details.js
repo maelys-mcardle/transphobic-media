487ed65661e62442dac8b38669d1e55ea9891f34
Maëlys McArdle <maelys@localhost.localdomain>
2018-05-20 19:25:38 -0400

    Improved description.

--- src/App/List/ListItem/Details/Details.js
+++ src/App/List/ListItem/Details/Details.js
@@ -83,7 +83,7 @@ export default class Details extends Component {
   renderTransPlayedText(transPlayedByCis) {
     switch (transPlayedByCis) {
       case null: 
-        return 'Contains no trans/non-binary roles to play (eg. stand-up comedy, documentary)';
+        return 'Contains no trans/non-binary roles to portray (eg. stand-up comedy, documentary)';
       case false: 
         return 'Trans/non-binary characters are portrayed by trans actors';
       case true: 
